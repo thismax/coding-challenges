3603a369a79f0b5ed57433c402b91d1adb6feb34
thismax <max.g.jacobs@gmail.com>
2017-11-16 10:12:46 -0800

    added problem

---
+++ javascript/DFS-BFS/traverseTree.js
@@ -0,0 +1,21 @@
+// Definition for binary tree:
+// function Tree(x) {
+//   this.value = x;
+//   this.left = null;
+//   this.right = null;
+// }
+// 
+// For the following tree:
+// 
+//     1
+//   /   \
+//  2     4
+//   \   /
+//    3 5
+// 
+// output => [1,2,4,3,5]
+// 
+// 
+function traverseTree(t) {
+  
+}
