e50bea89d508d74598662c76bda28d992cb6cf5c
thismax <max.g.jacobs@gmail.com>
2017-12-31 12:10:42 -0800

    adding shuffle

--- javascript/deckOfCards/cards.js
+++ javascript/deckOfCards/cards.js
@@ -1,18 +1,34 @@
-function card(value, name, suit) {
+function Card(value, name, suit) {
   this.value = value;
   this.name = name;
   this.suit = suit;
 }
 
-function deck() {
+function Deck() {
   this.names = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
   this.suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
-  let cards = [];
+  this.cards = [];
 
   this.names.forEach(name => {
     this.suits.forEach(suit => {
-      cards.push(`${name} of ${suit}`);
+      this.cards.push(`${name} of ${suit}`);
     });
   });
-  return cards;
+  return this.cards;
 }
+
+Deck.prototype.shuffle = function() {
+  let cards = this.cards;
+  let n = cards.length,
+    i,
+    t;
+  while (n) {
+    i = Math.floor(Math.random() * n);
+    n--;
+    t = cards[n];
+    cards[n] = cards[i];
+    cards[i] = t;
+  }
+  console.log(deck);
+  return deck;
+};
