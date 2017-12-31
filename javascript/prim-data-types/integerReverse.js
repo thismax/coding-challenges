e1fa61a5e5ffea14c5efbd0fdf7c32e6c01381c8
thismax <max.g.jacobs@gmail.com>
2017-12-31 11:17:24 -0800

    init

---
+++ javascript/prim-data-types/integerReverse.js
@@ -0,0 +1,20 @@
+/*                                      
+                       _   _             
+                      | | (_)            
+  __ _ _   _  ___  ___| |_ _  ___  _ __  
+ / _` | | | |/ _ \/ __| __| |/ _ \| '_ \ 
+| (_| | |_| |  __/\__ \ |_| | (_) | | | |
+ \__, |\__,_|\___||___/\__|_|\___/|_| |_|
+    | |                                  
+    |_|       
+ 
+Given a positive integer n, return its digits reversed.
+DO NOT CONVERT n INTO A STRING OR ARRAY.
+Only use integers and math in your solution.
+
+*/
+
+
+reverseInteger (n) => {
+// your code here
+}
