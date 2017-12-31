409c3b64760bddae82f947abcb72dc4338b09bfb
thismax <max.g.jacobs@gmail.com>
2017-12-31 12:19:37 -0800

    refactored to es6

--- javascript/deckOfCards/cards.js
+++ javascript/deckOfCards/cards.js
@@ -1,34 +1,31 @@
-function Card(value, name, suit) {
-  this.value = value;
-  this.name = name;
-  this.suit = suit;
-}
+class Deck {
+  constructor() {
+    this.names = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    this.suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
+    this.cards = [];
 
-function Deck() {
-  this.names = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-  this.suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
-  this.cards = [];
-
-  this.names.forEach(name => {
-    this.suits.forEach(suit => {
-      this.cards.push(`${name} of ${suit}`);
+    this.names.forEach(name => {
+      this.suits.forEach(suit => {
+        this.cards.push(`${name} of ${suit}`);
+      });
     });
-  });
-  return this.cards;
+  }
+
+  shuffle() {
+    let cards = this.cards;
+    let n = cards.length,
+      i,
+      t;
+    while (n) {
+      i = Math.floor(Math.random() * n);
+      n--;
+      t = cards[n];
+      cards[n] = cards[i];
+      cards[i] = t;
+    }
+    console.log(cards);
+    return cards;
+  }
 }
 
-Deck.prototype.shuffle = function() {
-  let cards = this.cards;
-  let n = cards.length,
-    i,
-    t;
-  while (n) {
-    i = Math.floor(Math.random() * n);
-    n--;
-    t = cards[n];
-    cards[n] = cards[i];
-    cards[i] = t;
-  }
-  console.log(deck);
-  return deck;
-};
+let deck = new Deck();
